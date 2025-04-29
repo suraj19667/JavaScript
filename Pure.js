d21dd50d983df9a5aa80b97a97656d4bfca0aa6f
Suraj Yadav <sy083609@gmail.com>
2025-04-29 11:27:12 +0530

    updated commit

---
+++ Pure.js
@@ -0,0 +1,25 @@
+// let btn=document.querySelector("#btn")
+
+// btn.addEventListener('click', ()=>{
+    
+//     alert("Welcome !")
+// })
+
+// let inp=document.querySelector("#inp")
+
+// inp=addEventListener('keydown', (e)=>{
+
+//     document.querySelector("#inp").value 
+
+
+// })
+
+let head=document.createElement('h1')
+
+head.innerText="Hello Javascript"
+
+let Box=document.querySelector("#box")
+Box.appendChild(head)
+
+
+//To-Do list by javascript

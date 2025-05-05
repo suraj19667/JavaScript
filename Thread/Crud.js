8da887294f17b2a2aaed260e4e69c19addf8c630
Suraj Yadav <sy083609@gmail.com>
2025-05-05 23:09:14 +0530

    updated commit

---
+++ Thread/Crud.js
@@ -0,0 +1,36 @@
+let fetchData = async () => {
+    let url = "http://localhost:3000/Hotel"
+  
+    let res = await fetch(url);
+    let data = await res.json();
+  
+    console.log(data);
+  
+    let datashow = document.querySelector("#datashow")
+  
+    data.map((e) => {
+      datashow.innerHTML += `
+        <tr>
+          <td>${e.Name}</td>
+          <td>${e.Age}</td>
+          <td>${e.Checking}</td>
+          <td>${e.Checkout}</td>
+          <td>${e.City}</td>
+          <td>${e.Person}</td>
+          <td>${e.Price}</td>
+
+          <td onclick="Del('${e.id}')">Delete</td>
+
+        </tr>
+      `
+    })
+  }
+  
+  let Del=(id)=>{
+
+let url = `http://localhost:3000/Hotel/${id}`
+
+fetch(url,{method:"DELETE"})
+
+  }
+fetchData();

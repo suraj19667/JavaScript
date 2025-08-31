32fbea0be109c3ce07d701c56fb0d2bc25de7948
Suraj Yadav <sy083609@gmail.com>
2025-08-31 09:08:33 +0530

    updated commit

--- otp.js
+++ otp.js
@@ -1,4 +1,4 @@
 function generateOTP() {
   let otp = Math.floor(100000 + Math.random() * 900000); // 6-digit OTP
-  document.getElementById("otpDisplay").innerText = "Your OTP: " + otp;
+  document.getElementById("otpDisplay").innerText = "Your OTP is: " + otp;
 }
